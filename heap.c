4e5f064ab18c3ac3485e4bd69175eae37e6c3fdb
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2017-09-15 18:40:06 -0400

    Reduce the code footprint of siftdown()
    Add siftup()
    
    	modified:   heap.c

--- heap.c
+++ heap.c
@@ -1,51 +1,66 @@
 #include <stdio.h>
 
 int x[] = {0, 18, 20, 15, 29, 23, 17, 22, 35, 40, 26, 51, 19};
+int y[] = {0, 12, 20, 15, 29, 23, 17, 22, 35, 40, 26, 51, 19, 13};
+
+void siftup(int n)
+{
+	int i, j, temp;
+
+	i = n;
+	temp = y[i];
+	while (i > 1) {
+		j = i / 2;
+
+		if (y[j] <= temp)
+			break;
+
+		y[i] = y[j];
+		i = j;
+	}
+	y[i] = temp;
+}
+
 
 void siftdown(int n)
 {
-	int i, j, k, temp;
+	int i, j, temp;
 
 	i = 1;
 	temp = x[i];
 	for (;;) {
 		j = 2 * i;
-		k = i;
 
 		if (j > n)
 			break;
 
-		if (temp > x[j])
-			k = j;
+		if ((j + 1 <= n) && (x[j + 1] < x[j]))
+			j++;
 
-		j++;
-		if (j > n) {
-			if (k != i)
-				x[i] = x[k];
-			break;
-		}
-
-		if ((temp > x[j]) && (x[j - 1] > x[j]))
-			k = j;
-		
-		if (k == i)
+		if (temp <= x[j])
 			break;
 
-		x[i] = x[k];
-		i = k;
+		x[i] = x[j];
+		i = j;
 	}
-	x[k] = temp;
+	x[i] = temp;
 }
 
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int n = sizeof(x) / sizeof(int) - 1;
+	int i, n;
 
+	n = sizeof(x) / sizeof(int) - 1;
 	siftdown(n);
 	for (i = 1; i <= n; i++)
 		printf("%3d\n", x[i]);
+	printf("\n");
+
+	n = sizeof(y) / sizeof(int) - 1;
+	siftup(n);
+	for (i = 1; i <= n; i++)
+		printf("%3d\n", y[i]);
 
 	return 0;
 }
